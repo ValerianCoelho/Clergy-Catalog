89682dd90a6f7998f65f699a992b8cbeab0ef531
Valerian Coelho <valerian27012004@gmail.com>
2024-01-31 20:25:21 +0530

    Basic Form Grid Setup

--- src/pages/Add/Add.jsx
+++ src/pages/Add/Add.jsx
@@ -1,7 +1,29 @@
+import { TextField, Grid, Typography } from "@mui/material";
+
 function Add() {
     return (
       <>
-        Add
+        <Grid container spacing={2}>
+          <Grid item sm={12} md={6} lg={4} xl={3}>
+            <TextField id="outlined-basic" label="First Name" variant="outlined" type="email" fullWidth={'auto'}/>
+          </Grid>
+          <Grid item sm={12} md={6} lg={4} xl={3}>
+            <TextField id="outlined-basic" label="First Name" variant="outlined" type="email" fullWidth={'auto'}/>
+          </Grid>
+          <Grid item sm={12} md={6} lg={4} xl={3}>
+            <TextField id="outlined-basic" label="First Name" variant="outlined" type="email" fullWidth={'auto'}/>
+          </Grid>
+          <Grid item sm={12} md={6} lg={4} xl={3}>
+            <TextField id="outlined-basic" label="First Name" variant="outlined" type="email" fullWidth={'auto'}/>
+          </Grid>
+          <Grid item sm={12} md={6} lg={4} xl={3}>
+            <TextField id="outlined-basic" label="First Name" variant="outlined" type="email" fullWidth={'auto'}/>
+          </Grid>
+          <Grid item sm={12} md={6} lg={4} xl={3}>
+            <TextField id="outlined-basic" label="First Name" variant="outlined" type="email" fullWidth={'auto'}/>
+          </Grid>
+          
+        </Grid>
       </>
     )
   }
