c39a60af4d1698657d3204f4df1d9769210f1384
Valerian Coelho <valerian27012004@gmail.com>
2024-02-02 21:28:02 +0530

    Expandable Row Added

--- src/pages/View/View.jsx
+++ src/pages/View/View.jsx
@@ -8,13 +8,20 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
 import { details } from './constants'
+import { Box, Collapse, IconButton } from '@mui/material';
+import { KeyboardArrowDown, KeyboardArrowUp } from '@mui/icons-material';
+
+import { useState } from 'react';
 
 export default function View() {
+  const [open, setOpen] = useState(-1);
+
   return (
     <TableContainer component={Paper}>
       <Table>
         <TableHead>
           <TableRow>
+            <TableCell>A</TableCell>
             <TableCell>First Name</TableCell>
             <TableCell>Last Name</TableCell>
             <TableCell>SBN</TableCell>
@@ -22,11 +29,27 @@ export default function View() {
         </TableHead>
         <TableBody>
           {details.map((person, index) => (
-            <TableRow key={index}>
-              <TableCell>{person.fname}</TableCell>
-              <TableCell>{person.lname}</TableCell>
-              <TableCell>{person.sbn}</TableCell>
-            </TableRow>
+            <React.Fragment key={index}>
+              <TableRow>
+                <TableCell>
+                  <IconButton onClick={()=>{setOpen(open === index ? -1 : index)}}>
+                    { open == index ? <KeyboardArrowUp/> : <KeyboardArrowDown/> }
+                  </IconButton>  
+                </TableCell>
+                <TableCell>{person.fname}</TableCell>
+                <TableCell>{person.lname}</TableCell>
+                <TableCell>{person.sbn}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell colSpan={4} sx={{paddingTop: 0, paddingBottom: 0}}>
+                  <Collapse in={open == index} timeout='auto' unmountOnExit>
+                    <Box>
+                      Its me
+                    </Box>
+                  </Collapse>
+                </TableCell>
+              </TableRow>
+            </React.Fragment>
           ))}
         </TableBody>
       </Table>
