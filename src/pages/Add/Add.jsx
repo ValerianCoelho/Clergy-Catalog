95850cbe2b2e629709b650f05c4ed66307f8d48f
Valerian Coelho <valerian27012004@gmail.com>
2024-02-04 19:04:29 +0530

    All Elements Rendered Dynamically

--- src/pages/Add/Add.jsx
+++ src/pages/Add/Add.jsx
@@ -1,24 +1,21 @@
-import {
-  TextField,
-  Grid,
-  Select,
-  MenuItem,
-  InputLabel,
-  Box,
-  Button,
-  Typography,
-  IconButton,
-  Stack,
-  ListItemAvatar,
-  Avatar,
-} from "@mui/material";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { useState } from "react";
+import { inputStructure } from "./constants";
 import Heading from "../../components/Heading/Heading";
-import { useEffect, useState } from "react";
 // import db from "../../backend/database";
 
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import MenuItem from "@mui/material/MenuItem";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import Stack from "@mui/material/Stack";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
 import RemoveIcon from "@mui/icons-material/Remove";
 import AddIcon from "@mui/icons-material/Add";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 function Title() {
   return (
@@ -79,10 +76,10 @@ function Add() {
     donations: [
       {
         purpose: "",
-        donationAmount: "",
+        amount: "",
         paymentMode: "",
         date: "",
-        receiptNo: "",
+        receipt: "",
       },
     ],
   });
@@ -107,216 +104,74 @@ function Add() {
     }
   };
 
-  useEffect(() => {
-    console.log(formData);
-  }, [formData]);
-
   return (
     <>
       <Heading title={"Create New Record"} />
       <Grid container spacing={2}>
-        <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
-          <TextField
-            id="fname"
-            label="First Name"
-            variant="outlined"
-            type="text"
-            fullWidth={true}
-            onChange={(e) => {
-              handleChange("fname", e.target.value);
-            }}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
-          <TextField
-            id="lname"
-            label="Last Name"
-            variant="outlined"
-            type="text"
-            fullWidth={true}
-            onChange={(e) => {
-              handleChange("lname", e.target.value);
-            }}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
-          <TextField
-            id="email"
-            label="Email"
-            variant="outlined"
-            type="text"
-            fullWidth={true}
-            onChange={(e) => {
-              handleChange("email", e.target.value);
-            }}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
-          <TextField
-            id="contact-no-1"
-            label="Contact No 1"
-            variant="outlined"
-            type="number"
-            fullWidth={true}
-            onChange={(e) => {
-              handleChange("contact1", e.target.value);
-            }}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
-          <TextField
-            id="contact-no-2"
-            label="Contact No 2"
-            variant="outlined"
-            type="number"
-            fullWidth={true}
-            onChange={(e) => {
-              handleChange("contact2", e.target.value);
-            }}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
-          <TextField
-            id="contact-no-3"
-            label="Contact No 3"
-            variant="outlined"
-            type="number"
-            fullWidth={true}
-            onChange={(e) => {
-              handleChange("contact3", e.target.value);
-            }}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
-          <TextField
-            id="pan"
-            label="PAN Number"
-            variant="outlined"
-            type="number"
-            fullWidth={true}
-            onChange={(e) => {
-              handleChange("pan", e.target.value);
-            }}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
-          <TextField
-            id="sbn"
-            label="SBN"
-            variant="outlined"
-            type="number"
-            fullWidth={true}
-            onChange={(e) => {
-              handleChange("sbn", e.target.value);
-            }}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
-          <TextField
-            id="beneficiary-1"
-            label="Beneficiary 1"
-            variant="outlined"
-            type="text"
-            fullWidth={true}
-            onChange={(e) => {
-              handleChange("beneficiary1", e.target.value);
-            }}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
-          <TextField
-            id="beneficiary-2"
-            label="Beneficiary 2"
-            variant="outlined"
-            type="text"
-            fullWidth={true}
-            onChange={(e) => {
-              handleChange("beneficiary2", e.target.value);
-            }}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
-          <TextField
-            id="address"
-            label="Address"
-            variant="outlined"
-            type="text"
-            fullWidth={true}
-            multiline
-            maxRows={3}
-            onChange={(e) => {
-              handleChange("address", e.target.value);
-            }}
-          />
-        </Grid>
+        {inputStructure.user.map(({ id, label, type }) => (
+          <Grid item xs={12} sm={6} md={6} lg={6} xl={6} key={id}>
+            <TextField
+              id={id}
+              label={label}
+              variant="outlined"
+              type={type}
+              fullWidth={true}
+              onChange={(e) => {
+                handleChange(id, e.target.value);
+              }}
+            />
+          </Grid>
+        ))}
       </Grid>
+
       <Heading title={<Title />} />
       <Grid container spacing={2}>
-        <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
-          <TextField
-            id="purpose"
-            label="Purpose"
-            variant="outlined"
-            type="text"
-            fullWidth={true}
-            onChange={(e) => {
-              handleChange("purpose", e.target.value, true, 0);
-            }}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
-          <TextField
-            id="amount"
-            label="Donation Amount"
-            variant="outlined"
-            type="number"
-            fullWidth={true}
-            onChange={(e) => {
-              handleChange("amount", e.target.value, true, 0);
-            }}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
-          <Box>
-            <TextField
-              label="Select"
-              fullWidth={true}
-              select
-              value={formData.donations[0].paymentMode}
-              onChange={(e) => {
-                handleChange("paymentMode", e.target.value, true, 0);
-              }}
-            >
-              <MenuItem value="India">India</MenuItem>
-              <MenuItem value="China">China</MenuItem>
-              <MenuItem value="America">America</MenuItem>
-            </TextField>
-          </Box>
-        </Grid>
-        <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
-          <DatePicker
-            label="Basic date picker"
-            sx={{ width: "100%" }}
-            onChange={(e) => {
-              const date = new Date(e['$d']);
-              const value = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
-              handleChange("date", value, true, 0);
-            }}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
-          <TextField
-            id="recieptNo"
-            label="Reciept Number"
-            variant="outlined"
-            type="number"
-            fullWidth={true}
-            onChange={(e) => {
-              handleChange("receiptNo", e.target.value, true, 0);
-            }}
-          />
-        </Grid>
+        {inputStructure.donations.map(({ id, label, type }) => (
+          <Grid item xs={12} sm={6} md={6} lg={6} xl={6} key={id}>
+            {type === "select" && (
+              <TextField
+                select
+                label={label}
+                fullWidth={true}
+                value={formData.donations[0].paymentMode}
+                onChange={(e) => {
+                  handleChange(id, e.target.value, true, 0);
+                }}
+              >
+                <MenuItem value="cheque">Cheque</MenuItem>
+                <MenuItem value="card">Card</MenuItem>
+                <MenuItem value="cash">Cash</MenuItem>
+              </TextField>
+            )}
+            {type === "date" && (
+              <DatePicker
+                label={label}
+                sx={{ width: "100%" }}
+                onChange={(e) => {
+                  const date = new Date(e["$d"]);
+                  const value = `${date.getDate()}/${
+                    date.getMonth() + 1
+                  }/${date.getFullYear()}`;
+                  handleChange(id, value, true, 0);
+                }}
+              />
+            )}
+            {(type === "text" || type === "number") && (
+              <TextField
+                id={id}
+                label={label}
+                variant="outlined"
+                type={type}
+                fullWidth={true}
+                onChange={(e) => {
+                  handleChange(id, e.target.value, true, 0);
+                }}
+              />
+            )}
+          </Grid>
+        ))}
       </Grid>
+
       <Button variant="contained" fullWidth={true} sx={{ my: 2 }}>
         Add Record
       </Button>
---
+++ src/pages/Add/constants.js
@@ -0,0 +1,86 @@
+export const inputStructure = {
+    user: [
+      {
+        id: "fname",
+        label: "First Name",
+        type: "text",
+      },
+      {
+        id: "lname",
+        label: "Last Name",
+        type: "text",
+      },
+      {
+        id: "email",
+        label: "Email",
+        type: "text",
+      },
+      {
+        id: "contact1",
+        label: "Contact No 1",
+        type: "number",
+      },
+      {
+        id: "contact2",
+        label: "Contact No 2",
+        type: "number",
+      },
+      {
+        id: "contact3",
+        label: "Contact No 3",
+        type: "number",
+      },
+      {
+        id: "pan",
+        label: "PAN Number",
+        type: "number",
+      },
+      {
+        id: "sbn",
+        label: "SBN",
+        type: "number",
+      },
+      {
+        id: "beneficiary1",
+        label: "Beneficiary 1",
+        type: "text",
+      },
+      {
+        id: "beneficiary2",
+        label: "Beneficiary 2",
+        type: "text",
+      },
+      {
+        id: "address",
+        label: "Address",
+        type: "text",
+      },
+    ],
+    donations: [
+      {
+        id: "purpose",
+        label: "Purpose",
+        type: "text",
+      },
+      {
+        id: "amount",
+        label: "Donation Amount",
+        type: "number",
+      },
+      {
+        id: "paymentMode",
+        label: "Payment Mode",
+        type: "select",
+      },
+      {
+        id: "date",
+        label: "Date",
+        type: "date",
+      },
+      {
+        id: "receipt",
+        label: "Receipt Number",
+        type: "number",
+      },
+    ],
+  };
