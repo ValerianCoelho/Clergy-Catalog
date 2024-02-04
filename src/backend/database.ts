ab042b19e9fe8d559a172ba781e930813580fc52
danferns <dannywritescode@gmail.com>
2024-02-04 10:48:31 +0530

    add example table

--- src/backend/database.ts
+++ src/backend/database.ts
@@ -1,7 +1,10 @@
-
 import Database from "tauri-plugin-sql-api";
 
 // sqlite. The path is relative to `tauri::api::path::BaseDirectory::App`.
 const db = await Database.load("sqlite:test.db");
+
+// create tables like this
+
+db.execute("create table if not exists users (name varchar(20), age int(5));");
 
 export default db;
