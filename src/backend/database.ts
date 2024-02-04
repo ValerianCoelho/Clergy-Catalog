259ccd17e53f1e485ea364bfdad8b81104ff7c05
Valerian Coelho <valerian27012004@gmail.com>
2024-02-04 16:52:50 +0530

    Buttons Added To Add Record Page

--- src/backend/database.ts
+++ src/backend/database.ts
@@ -3,8 +3,4 @@ import Database from "tauri-plugin-sql-api";
 // sqlite. The path is relative to `tauri::api::path::BaseDirectory::App`.
 const db = await Database.load("sqlite:test.db");
 
-// create tables like this
-
-db.execute("create table if not exists users (name varchar(20), age int(5));");
-
 export default db;
--- src/pages/Add/Add.jsx
+++ src/pages/Add/Add.jsx
@@ -1,8 +1,36 @@
-import { TextField, Grid, Select, MenuItem, InputLabel, Box, Button } from "@mui/material";
+import { TextField, Grid, Select, MenuItem, InputLabel, Box, Button, Typography, IconButton, Stack, ListItemAvatar, Avatar } from "@mui/material";
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import Heading from "../../components/Heading/Heading";
 import { useState } from 'react';
-import db from "../../backend/database";
+// import db from "../../backend/database";
+
+import RemoveIcon from '@mui/icons-material/Remove';
+import AddIcon from '@mui/icons-material/Add';
+
+function Title() {
+  return (
+    <>
+      <Stack direction={'row'} alignItems={'center'} spacing={2}>
+      <Typography variant="h5">Donations</Typography>
+      <ListItemAvatar>
+        <Stack direction={'row'} spacing={1} alignItems={'center'}>
+          <Avatar sx={{width: 32, height: 32, backgroundColor: 'rgba(64, 192, 87, .1)'}}>
+            <IconButton>
+              <AddIcon fontSize="small" sx={{color: 'rgba(64, 192, 87, 1)'}}/>
+            </IconButton>
+          </Avatar>
+          <Avatar  sx={{width: 32, height: 32, backgroundColor: 'rgba(192, 64, 64, .1)'}}>
+            <IconButton>
+              <RemoveIcon fontSize="small" color="error" sx={{color: 'rgb(192, 64, 64)'}}/>
+            </IconButton>
+          </Avatar>
+        </Stack>
+      </ListItemAvatar>
+      
+      </Stack>
+    </>
+  )
+}
 
 function Add() {
   const [country, setCountry] = useState('');
@@ -11,12 +39,14 @@ function Add() {
     setCountry(event.target.value);
   };
 
-  async function handleClick() {
-    const result = await db.execute(
-      "INSERT INTO users (name, age) VALUES ('Valerian Coelho', 20);",
-    );
-    console.log(result)
-  }
+  // async function handleClick() {
+  //   const result = await db.execute(
+  //     `
+  //     `,
+  //   );
+
+  //   console.log(result)
+  // }
 
     return (
       <>
@@ -56,7 +86,7 @@ function Add() {
             <TextField id="address" label="Address" variant="outlined" type="text" fullWidth={true} multiline maxRows={3}/>
           </Grid>
         </Grid>
-        <Heading title={'Donation 1'}/>
+        <Heading title={<Title/>}/>
         <Grid container spacing={2}>
           <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
             <TextField id="purpose" label="Purpose" variant="outlined" type="text" fullWidth={true}/>
@@ -82,7 +112,7 @@ function Add() {
             <TextField id="reciept-no" label="Reciept Number" variant="outlined" type="number" fullWidth={true}/>
           </Grid>
         </Grid>
-        <Button variant="contained" onClick={handleClick}>Hello</Button>
+        <Button variant="contained" fullWidth={true} sx={{my: 2}}>Add Record</Button>
       </>
     )
   }
