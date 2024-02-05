b764dea517592c3c9d9e44fe2f70c45db97855b3
Valerian Coelho <valerian27012004@gmail.com>
2024-02-05 22:52:11 +0530

    Import Statements Cleaned

--- src/pages/View/View.jsx
+++ src/pages/View/View.jsx
@@ -1,62 +1,59 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { details } from "./constants";
+import db from "../../backend/database";
+
+import Heading from "../../components/Heading/Heading";
+import DisplayDonations from "./components/DisplayDonations/DisplayDonations";
+import DisplayAdditionalDetails from "./components/DisplayAdditionalDetails/DisplayAdditionalDetails";
+
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+
 import Paper from "@mui/material/Paper";
+import MenuItem from "@mui/material/MenuItem";
+import Stack from "@mui/material/Stack";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
 
-import { styled } from "@mui/material/styles";
-
-import { details } from "./constants";
-import {
-  MenuItem,
-  Stack,
-  Button,
-  Collapse,
-  IconButton,
-  TextField,
-  Typography,
-} from "@mui/material";
-import { KeyboardArrowDown, KeyboardArrowUp } from "@mui/icons-material";
-
-import DisplayDonations from "./components/DisplayDonations/DisplayDonations";
-import DisplayAdditionalDetails from "./components/DisplayAdditionalDetails/DisplayAdditionalDetails";
-import { useState } from "react";
-import db from "../../backend/database";
-
-import Heading from "../../components/Heading/Heading";
-
-async function fetchDetails() {
-  const people = await db.select("SELECT * FROM person;");
-  const donations = await db.select("SELECT * FROM donation;");
-
-  const details = [];
-  for (const person of people) {
-    // combine both beneficiaries in UI
-    person.beneficiary =
-      person.beneficiary1 +
-      (person.beneficiary2 ? `, ${person.beneficiary2}` : "");
-
-    person.donations = [];
-    for (const donation of donations) {
-      if (donation.sbn != person.sbn) continue;
-      person.donations.push(donation);
-    }
-
-    details.push(person);
-  }
-
-  console.log(details);
-  return details;
-}
+import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUp from "@mui/icons-material/KeyboardArrowUp";
 
 export default function View() {
   const [open, setOpen] = useState(-1);
-  const [searchAttribute, setSearchAttribute] = useState('First Name')
+  const [searchAttribute, setSearchAttribute] = useState('First Name');
 
-  fetchDetails();
+  useEffect(()=>{
+    fetchDetails();
+  }, [])
+
+  async function fetchDetails() {
+    const people = await db.select("SELECT * FROM person;");
+    const donations = await db.select("SELECT * FROM donation;");
+  
+    const details = [];
+    for (const person of people) {
+      // combine both beneficiaries in UI
+      person.beneficiary =
+        person.beneficiary1 +
+        (person.beneficiary2 ? `, ${person.beneficiary2}` : "");
+  
+      person.donations = [];
+      for (const donation of donations) {
+        if (donation.sbn != person.sbn) continue;
+        person.donations.push(donation);
+      }
+      details.push(person);
+    }
+    console.log(details);
+    return details;
+  }
 
   return (
     <>
@@ -68,8 +65,12 @@ export default function View() {
           type="text"
           fullWidth={true}
         />
-        <TextField select label={"Select"} value={searchAttribute} sx={{minWidth: 200}}
-        onChange={(e)=>(setSearchAttribute(e.target.value))}>
+        <TextField 
+          select label={"Select"} 
+          value={searchAttribute} 
+          sx={{minWidth: 200}}
+          onChange={(e)=>(setSearchAttribute(e.target.value))}
+        >
           <MenuItem value="First Name">First Name</MenuItem>
           <MenuItem value="Last Name">Last Name</MenuItem>
           <MenuItem value="SBN">SBN</MenuItem>
