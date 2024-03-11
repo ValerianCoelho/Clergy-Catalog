735a33d62a8076360ca79ada382a01c52e517289
Valerian Coelho <valerian27012004@gmail.com>
2024-03-11 11:27:03 +0530

    Database UX Improved

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -13,6 +13,8 @@ import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 
 import CreateDialog from "../../components/Dialog/CreateDialog/CreateDialog";
 
+import Database from "./components/Database/Database";
+
 function Settings() {
   const [createDbDialog, setCreateDbDialog] = useState(false);
   const [loadDbDialog, setLoadDbDialog] = useState(false);
@@ -24,65 +26,12 @@ function Settings() {
     scrollToTop();
   }, []);
 
-  const handleClose = () => {
-    setCreateDbDialog(false);
-  };
-
-  const handleCreateDB = () => {
-    setCreateDbDialog(true);
-  };
-
   return (
     <>
-      <Heading title={"Settings"} />
-      <Typography variant="body1" fontWeight={"bold"} mb={1}>
-        SETUP
-      </Typography>
-      <Stack spacing={2} direction={"row"}>
-        <ActionCard
-          icon={<ExitToAppIcon sx={{ color: "black" }} />}
-          title={"Create DB"}
-          description={
-            "Creates a db with the tables required to run the application"
-          }
-          actionTitle={"CREATE DATABASE"}
-          handleClick={handleCreateDB}
-        />
-        <ActionCard
-          icon={<ExitToAppIcon sx={{ color: "black" }} />}
-          title={"Load DB"}
-          description={"Loads the db from the root of the appliation for use"}
-          actionTitle={"LOAD DATABASE"}
-        />
-        <ActionCard
-          icon={<ExitToAppIcon sx={{ color: "black" }} />}
-          title={"Import DB"}
-          description={"Places the db in the root directory of the application"}
-          actionTitle={"IMPORT DATABASE"}
-        />
-      </Stack>
-      <Typography variant="body1" fontWeight={"bold"} mb={1} mt={4}>
-        BACKUP
-      </Typography>
-      <Stack spacing={2} direction={"row"}>
-        <ActionCard
-          icon={<SystemUpdateAltIcon sx={{ color: "black" }} />}
-          title={"Export DB"}
-          description={
-            "Export the Database from the root of the application to any folder on the computer"
-          }
-          actionTitle={"EXPORT DATABASE"}
-        />
-        <ActionCard
-          icon={<BackupTableIcon sx={{ color: "black" }} />}
-          title={"EXPORT AS CSV"}
-          description={"Export the Currently Loaded DB as a CSV File"}
-          actionTitle={"EXPORT AS CSV FILE"}
-        />
-      </Stack>
+      <Heading title={"Data Management"} />
+      <Database/>
       <Heading title={"Deleted Records"} />
       <DisplayDeleted />
-      <CreateDialog open={createDbDialog} handleClose={handleClose} />
     </>
   );
 }
---
+++ src/pages/Settings/components/Database/Database.jsx
@@ -0,0 +1,78 @@
+import * as React from "react";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import {
+  Box,
+  Button,
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Paper,
+  Typography,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import MoreVert from "@mui/icons-material/MoreVert";
+
+const options = [
+  "Set Active",
+  "Export (CSV)",
+  "Export DB File",
+  "Delete Database",
+];
+
+const databases = ["Database 1", "Database 2", "Database 3"];
+
+function Database() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Paper sx={{overflow: 'hidden'}}>
+        <Typography variant="h6" p={1} sx={{ backgroundColor: 'black', color: 'white' }}>
+          Databases
+        </Typography>
+        <Divider />
+        <Box px={0}>
+          {databases.map((database, index) => {
+            return (
+              <List disablePadding>
+                <ListItem>
+                  <ListItemText>{database}</ListItemText>
+                  <IconButton onClick={handleClick}>
+                    <MoreVertIcon />
+                  </IconButton>
+                </ListItem>
+                <Divider />
+              </List>
+            );
+          })}
+        </Box>
+        <Box p={1}>
+          <Button variant="text" startIcon={<AddIcon />} fullWidth={true} disableElevation>
+            Create New
+          </Button>
+        </Box>
+      </Paper>
+      <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+        {options.map((option) => (
+          <MenuItem key={option} onClick={handleClose}>
+            {option}
+          </MenuItem>
+        ))}
+      </Menu>
+    </div>
+  );
+}
+
+export default Database;
